5d0daa6dce76ffdc619878ba11cb2cf299815f85
Brang <brangtsawmaung89@gmail.com>
2024-06-28 01:51:22 +0630

    update login mobile view2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -73,23 +73,24 @@ const Login = () => {
   return (
     <div className='w-full h-full flex items-center justify-center'>
       <SlideLeftMotion>
-        <div className='card w-[100%] h-[430px] sm:w-[70%] lg:w-[700px] lg:h-[470px] bg-white shadow-lg flex flex-col md:flex-row items-center justify-center rounded-tl-[60px] rounded-tr-[60px] md:rounded-[50px] overflow-hidden'>
+        <div className='card w-[100%] h-[480px] sm:w-[80%] lg:w-[700px] lg:h-[470px] bg-white shadow-lg flex flex-col md:flex-row items-center justify-center rounded-tl-[50px] rounded-tr-[50px] md:rounded-[50px] overflow-hidden'>
           <div className="left w-full md:w-7/12 h-full flex items-center justify-center p-6">
-            <div className="form flex flex-col w-full p-10 items-center justify-center gap-8 md:gap-6">
+            <div className="form flex flex-col w-full px-10 md:p-10 items-center justify-center gap-8 md:gap-6">
               <h1 className='hidden md:flex uppercase text-primary-800 text-2xl font-semibold'>Sign In</h1>
-              <div className="email flex flex-col gap-2 w-full text-primary-800">
-                <label htmlFor="email" className='flex gap-2 items-center'><MdOutlineEmail /> Email</label>
-                <input type="email" id='email' ref={emailRef} placeholder='Enter your email' className='border-l-4 bg-gray-100 border-primary-400 w-full h-12 p-3 rounded-sm outline-none' onChange={(e) => setInputData({...inputData, email: e.target.value})}/>
+              <div className="email flex flex-col gap-3 md:gap-2 w-full text-primary-800">
+                <label htmlFor="email" className='flex text-lg gap-2 items-center'><MdOutlineEmail /> Email</label>
+                <input type="email" id='email' ref={emailRef} placeholder='Enter your email' className='border-l-4 bg-gray-100 border-primary-400 w-full h-14 md:h-12 p-3 rounded-sm outline-none' onChange={(e) => setInputData({...inputData, email: e.target.value})}/>
               </div>
-              <div className="password flex flex-col gap-2 w-full text-primary-800">
-                <label htmlFor="password" className='flex gap-2 items-center'><MdLockOutline /> Password</label>
+              <div className="password flex flex-col gap-3 md:gap-2 w-full text-primary-800">
+                <label htmlFor="password" className='flex text-lg gap-2 items-center'><MdLockOutline /> Password</label>
                 <div className="input relative">
-                  <input type={showPassword ? 'text' : "password"} id='password' ref={passwordRef} placeholder='Enter your password' className='border-l-4 bg-gray-100 border-primary-400 w-full h-12 p-3 rounded-sm outline-none' onChange={(e) => setInputData({...inputData, password: e.target.value})}/>
+                  <input type={showPassword ? 'text' : "password"} id='password' ref={passwordRef} placeholder='Enter your password' className='border-l-4 bg-gray-100 border-primary-400 w-full h-14 md:h-12 p-3 rounded-sm outline-none' onChange={(e) => setInputData({...inputData, password: e.target.value})}/>
                   {showPassword ? <FaEye className='absolute top-4 right-3 text-lg cursor-pointer' onClick={() => setShowPassword(!showPassword)}/> : <FaEyeSlash className='absolute top-4 right-3 text-lg cursor-pointer' onClick={() => setShowPassword(!showPassword)}/>}
                 </div>
                 <p className='text-sm text-primary-600 font-medium cursor-pointer'>Forgot password?</p>
               </div>
               <button className='uppercase bg-primary-400 font-semibold hover:bg-primary-500 duration-200 flex items-center justify-center w-full h-12 text-primary-50' onClick={handleLogin}>{loading ? <BtnLogin />: 'Sign In'}</button>
+              <p className='block md:hidden'>If you don't have an account. <Link to={'/signup'} className='font-semibold text-primary-600'>Sign Up</Link></p>
             </div>
           </div>
           <div className="hidden right w-6/12 bg-primary-400 h-full rounded-tl-[150px] rounded-bl-[100px] md:flex flex-col items-center justify-center p-6 gap-3">
