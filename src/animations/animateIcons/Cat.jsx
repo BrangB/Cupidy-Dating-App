cbc7092ca75dfb04d48a593ea8a8e3bc9524d030
Brang <brangtsawmaung89@gmail.com>
2024-07-14 00:33:22 +0630

    fix cat

--- src/animations/animateIcons/Cat.jsx
+++ src/animations/animateIcons/Cat.jsx
@@ -4,7 +4,7 @@ import cat from '../../lotties/cat.json'
 
 const Cat = () => {
     return (
-        <div className='w-[170px] md:w-[200px] flex items-center justify-center absolute -bottom-9 right-5 md:right-32'>
+        <div className='w-[140px] md:w-[200px] flex items-center justify-center absolute -bottom-8 right-5 md:right-32'>
           <Lottie
             animationData={cat}
             className="flex justify-center items-center"
