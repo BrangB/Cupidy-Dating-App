c262b3e69aca9024742eb8c4b01b6bca4d2b6c24
Brang <brangtsawmaung89@gmail.com>
2024-08-26 16:41:27 +0630

    change language animation

--- src/animations/animateIcons/LanguageAnimation.jsx
+++ src/animations/animateIcons/LanguageAnimation.jsx
@@ -5,11 +5,12 @@ import Lottie from 'lottie-react'
 const LanguageAnimation = () => {
   return (
     <div style={{ width: '300px'}} className='flex items-center justify-center'>
-        <Lottie
+        {/* <Lottie
         animationData={loading}
         className="flex justify-center items-center"
         loop={true}
-        />
+        /> */}
+      <div className='text-2xl loading-image text-colortext-primary tracking-[6px] text-center'><span className='text-3xl'>C</span>hanging <span className='text-3xl'>L</span>anguage...</div>
   </div>
   )
 }
